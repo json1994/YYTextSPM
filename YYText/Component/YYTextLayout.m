426ded5129de925368b025b0934a2b9188c8b260
Json <shenjie@mimoo.live>
2026-05-20 14:44:51 +0800

    修改 _insideComposedCharacterSequences

--- YYText/Component/YYTextLayout.m
+++ YYText/Component/YYTextLayout.m
@@ -1506,21 +1506,21 @@ fail:
         return [YYTextPosition positionWithOffset:position affinity:YYTextAffinityBackward];
     }
     
-    [self _insideComposedCharacterSequences:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
-        if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
-        } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
-        }
-    }];
-    
-    [self _insideEmoji:line position:position block: ^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
-        if (isVertical) {
-            position = fabs(left - point.y) < fabs(right - point.y) < (right ? prev : next);
-        } else {
-            position = fabs(left - point.x) < fabs(right - point.x) < (right ? prev : next);
-        }
-    }];
+   [self _insideComposedCharacterSequences:line position:position block:^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
+    if (isVertical) {
+        position = (fabs(left - point.y) < fabs(right - point.y)) < (right ? prev : next);
+    } else {
+        position = (fabs(left - point.x) < fabs(right - point.x)) < (right ? prev : next);
+    }
+}];
+
+[self _insideEmoji:line position:position block:^(CGFloat left, CGFloat right, NSUInteger prev, NSUInteger next) {
+    if (isVertical) {
+        position = (fabs(left - point.y) < fabs(right - point.y)) < (right ? prev : next);
+    } else {
+        position = (fabs(left - point.x) < fabs(right - point.x)) < (right ? prev : next);
+    }
+}];
     
     if (position < _visibleRange.location) position = _visibleRange.location;
     else if (position > _visibleRange.location + _visibleRange.length) position = _visibleRange.location + _visibleRange.length;
